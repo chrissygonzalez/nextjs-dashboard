f25507293d583d3f7cf3b3ec596407a7eb5fb058
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-03-11 15:02:58 -0400

    Finish chapter 14

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-    extends: ['next/core-web-vitals', 'prettier'],
+    extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
     ignorePatterns: ['**/.next/**', '**/node_modules/**'],
     root: true,
+    rules: { "@typescript-eslint/no-unused-vars": "off", "@next/next/no-html-link-for-pages": "off" },
     settings: {
         next: {
             rootDir: ['basics/*/', 'dashboard/*/', 'seo/'],
